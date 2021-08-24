1919446e624be198f96dda55955c975b77494a04
tianfeichen <1143051193@qq.com>
2021-08-24 22:47:09 +0800

    20210824-2

---
+++ src/main/java/com/ctf/web/InfoServlet.java
@@ -0,0 +1,11 @@
+package com.ctf.web;
+
+/**
+ * @Description :
+ * @ClassName InfoServlet
+ * @Author tianfeichen
+ * @Date 2021/8/24 22:45
+ * @Version v1.0
+ */
+public class InfoServlet extends BaseServlet{
+}
